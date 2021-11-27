9cba667cc8dc2005db55737ab8188bcf03afdf3c
JackoDev <untaljacko@gmail.com>
2021-11-27 17:32:39 -0500

    course udemy for basics react

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../utils/getGifs'
+import React from 'react'
 import GifGridItem from './GifGridItem'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 
 export default function GifGrid ({category}) {
-  const [images, setImages] = useState([])
-
-  useEffect( () => {
-    getGifs(category)
-    .then(imgs => setImages(imgs))
-  }, [category])
+  const { data, loading } = useFetchGifs( category )
 
   return (
     <>
+    { loading && <p>Loading ...</p> }
     <h3> { category } </h3>
     <div className="card-grid">
-      { images.map( img => (
+      { data.map( img => (
         <GifGridItem
           key={img.id}
           id={img.id}
